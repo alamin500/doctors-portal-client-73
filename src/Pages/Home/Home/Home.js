3b978ec1a0fb44b147d957f87cd92ec2d9504640
alamin500 <nalamin500@gmail.com>
2021-11-07 23:01:52 +0600

    first

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -5,14 +5,14 @@ import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
 
 const Home = () => {
-    return (
-        <div>
-            <Navigation></Navigation>
-            <Banner></Banner>
-            <Services></Services>
-            <AppointmentBanner></AppointmentBanner>
-        </div>
-    );
+  return (
+    <div>
+      <Navigation></Navigation>
+      <Banner></Banner>
+      <Services></Services>
+      <AppointmentBanner></AppointmentBanner>
+    </div>
+  );
 };
 
 export default Home;
